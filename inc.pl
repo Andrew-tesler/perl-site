4c96d74aeca6afde2af9520fe7f42e9dc7484028
andrew-tesler <tesler.andrew@gmail.com>
2013-01-15 17:35:50 +0200

    inc.pl Main directory locations file

---
+++ inc.pl
@@ -0,0 +1,11 @@
+# Main directory file for log.pl site 
+
+$log_dir = "/var/opt/atp/drop-log/release/compulab/console/";
+$atp_dir = "/var/opt/atp";
+$bo_dir =  $atp_dir."/config/1";
+$bo_db = $bo_dir."/bo.db";
+
+@products = ("fit-PC3","SBC-fit-PC3","intense-PC","FM-LAN","FM-XTD","FM-4U" );
+@tests = ("All","CPU","USB","Audio","Thermal","NTP","Update","total time","MAC","Ethernet");
+
+
