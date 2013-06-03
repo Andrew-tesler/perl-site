a27e70e355cde2f1b7299d9367d437fb2b8f91e6
Andrew <tesler.andrew@gmail.com>
2013-06-03 11:28:04 +0300

    Removed all unused comments

--- inc.pl
+++ inc.pl
@@ -14,13 +14,6 @@ $bo_db = $bo_dir."bo.db";
 
 # The real locations on the server
 
-#$log_dir = "/var/opt/atp/drop-log/release/compulab/console/";
-#$atp_dir = "/var/opt/atp";
-#$bo_dir =  $atp_dir."/config/1";
 
-# products tested
-@products = ("fit-PC3","SBC-fit-PC3","intense-PC","FM-LAN","FM-XTD","FM-4U" );
-# Tested devices
-@tests = ("All","CPU","USB","Audio","Thermal","NTP","Update","total time","MAC","Ethernet");
 
 
--- log.pl
+++ log.pl
@@ -7,7 +7,7 @@ require "/var/www/s.log.pl";
 
 # create an instance of the CGI object 
 $cgiobject = new CGI;
-$greeting="Please enter your search ()";
+$greeting="Please enter your search";
 
 print $cgiobject->header;
 print $cgiobject->start_html(-title=>'ATP LOGS',-bgcolor=>'008980');
@@ -33,55 +33,7 @@ sub output_form()
    $theform.=$cgiobject->textfield(-name=>'serial_number',
                                    -size=>50,
                                    -default=>$search_term);
-   ##create error # search field
-   #$theform.="Search error#: ";
-   #$theform.=$cgiobject->textfield(-name=>'error_number',
-                                   #-size=>30,
-                                   #-default=>$search_term);                                
-                                              
-   ##create ignore gae 
-   #$theform.="<BR>Results type:";
-   #$theform.=$cgiobject->radio_group(-name=>'display_logs',
-                                     #-values=>["Display attempts","Hide attempts"],
-                                     #-default=>"Hide log files");
-                                   
-   ##create include all or only by SUCCES ERROR
-   #$theform.="<BR>Print log file:";
-   #$theform.=$cgiobject->radio_group(-name=>'print_file',
-                                     #-values=>["NO","Print file"],
-                                     #-default=>"NO");
-                                     
-    ##create select box for products 
-    #$theform.="<BR>Products<BR>";
-    #$theform.=$cgiobject->scrolling_list(-name=>'products',
-                                         #-values=>[@products],                                 
-                                         #-size=>8,
-                                        #-multiple=>'false');
-   ##Create search by devices choose box                                     
-   #$theform.="<BR>Errors:<BR>";  
-   #$theform.=$cgiobject-> scrolling_list(-name=>'tested_device',
-			            	#-values=>[@tests],
-			            	#-default=>['All'],
-				            #-size=>8,
-				            #-multiple=>'true',
-                            #-labels=>\%labels,
-                            #-attributes=>\%attributes);
-                            
-    ## Create search by batch lines                         
-    #$theform.="<BR>Batch Line<BR>";
-    #$theform.=$cgiobject->scrolling_list(-name=>'batch_line',
-                                         #-values=>[@batch],
-                                         #-default=>['0'],                                 
-                                         #-size=>15,
-                                         #-multiple=>'false');  
-                                         
-    ##Create quick summary buttons. 
-   #$theform.="<BR>Results type:";
-   #$theform.=$cgiobject->radio_group(-name=>'quick_summary',
-                                     #-values=>["Quick Summary","Table"],
-                                     #-default=>$ignore_gae);                                                           
-   
-   
+                                                            
    #create submit and reset buttons
    $theform.="<BR><BR>";
    $theform.=$cgiobject->submit(-label=>'Submit');
